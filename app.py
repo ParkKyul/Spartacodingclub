61a9caca728257152e141766d237586ece9a153c
ParkKyul <90172819+ParkKyul@users.noreply.github.com>
2021-09-23 16:58:53 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,38 +1,41 @@
+from pymongo import MongoClient
+
 from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
-from pymongo import MongoClient
-
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
-
-## HTML을 주는 부분
+# HTML 화면 보여주기
 @app.route('/')
 def home():
     return render_template('index.html')
 
+# API 역할을 하는 부분
+@app.route('/api/list', methods=['GET'])
+def show_stars():
+    movie_star = list(db.mystar.find({}, {'_id': False}).sort('like', -1))
+    return jsonify({'movie_stars': movie_star})
 
-## API 역할을 하는 부분
-@app.route('/review', methods=['POST', 'GET'])
-def write_review():
-    if request.method == "POST":
-        title_receive = request.form['title_give']
-        author_receive = request.form['author_give']
-        review_receive = request.form['review_give']
-        doc = {
-            'title': title_receive,
-            'author': author_receive,
-            'review': review_receive
-        }
-        db.bookreview.insert_one(doc)
-        return jsonify({'result': 'success', 'msg': '완료 예아'})
+@app.route('/api/like', methods=['POST'])
+def like_star():
+    name_receive = request.form['name_give']
 
-    elif request.method == "GET":
-        reviews = list(db.bookreview.find({}, {'_id': False}))
-        return jsonify({'all_reviews': reviews})
+    target_star = db.mystar.find_one({'name': name_receive})
+    current_like = target_star['like']
 
+    new_like = current_like + 1
+
+    db.mystar.update_one({'name': name_receive}, {'$set': {'like': new_like}})
+
+    return jsonify({'msg': '좋아요 완료!'})
+
+@app.route('/api/delete', methods=['POST'])
+def delete_star():
+    name_receive = request.form['name_give']
+    db.mystar.delete_one({'name': name_receive})
+    return jsonify({'msg': '삭제 완료!'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
